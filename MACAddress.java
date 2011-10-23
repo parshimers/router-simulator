0d3775c975185bacade2f37f5dc3f2216cbf49ca
Ian Maxon <parshimers@gmail.com>
2011-10-23 01:07:18 -0700

    MACAddress now takes and gives out correct values with byte arrays.

--- MACAddress.java
+++ MACAddress.java
@@ -16,13 +16,13 @@ public class MACAddress {
     }
     
     private long byteToLong(byte[] ary){
-        return
-        ((long)(ary[0] & 0xff) << 56) |
-        ((long)(ary[1] & 0xff) << 48) |
-        ((long)(ary[2] & 0xff) << 40) |
-        ((long)(ary[3] & 0xff) << 32) |
-        ((long)(ary[4] & 0xff) << 24) |
-        ((long)(ary[5] & 0xff) << 16);
+        long ln =0;
+        for(int i=0; i<5; i++){
+            ln |= ary[i] & 0xFF;
+            ln <<= 8;
+        }
+        ln |= ary[5] & 0xFF;
+        return ln;
     }
  
     public long getLongAddress() {
@@ -33,7 +33,7 @@ public class MACAddress {
         byte[] bytes = new byte[6];
         
         for( int i = 0; i < 6; i++ ) {
-            bytes[i] = (byte) ( (addr >> i*8) & 0xff );
+            bytes[i] = (byte)( (addr >> (40-(i*8))) & 0xff );
         }
         
         return bytes;
